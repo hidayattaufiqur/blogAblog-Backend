5359ef21cbb77532d87f607e406f48224f05156c
Hidayat Taufiqur Rahmah Achmad <67844213+hidayattaufiqur@users.noreply.github.com>
2021-04-25 05:16:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-mongoose.connect('mongodb+srv://2588:2588@cluster0.hch3f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+mongoose.connect('URI'), 
 { useNewUrlParser: true, useUnifiedTopology: true , useCreateIndex: true } )
 
 app.set('view engine', 'ejs')
@@ -17,31 +17,6 @@ app.use(bodyParser.urlencoded({ extended: false}))
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 app.use('/public', express.static('public'));
-
-// app.get('/', async (req, res) => {
-//     res.render('articles/index')
-// })
-
-// app.get('/aboutUs', async (req, res) => {
-//     res.render('articles/aboutUs')
-// })
-
-// app.post('/newMail', async (req, res, next) => {
-//     var form = new Form();
-//     form.email= req.body.email;
-    
-//     form.save(function(err, form) {
-//                 if (err) return next(err);
-//                     if (err) return next(err);
-//                    res.redirect("/aboutUs");
-//             });
-// })
-
-
-// app.get('/articles/blog', async (req, res) => {
-//     const articles = await Article.find().sort({createdAt: 'descending'})
-//     res.render('articles/blog', { articles: articles })
-// })
 
 app.use('/articles', articleRouter)
 app.use('/mail', Form)
