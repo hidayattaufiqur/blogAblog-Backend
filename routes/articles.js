280ef9819075b707df3e4c9e220c8f749045fb6d
hidayattaufiqur <htaufiqurrahma2001@gmail.com>
2021-05-04 21:08:29 +0800

    update

--- routes/articles.js
+++ routes/articles.js
@@ -2,9 +2,26 @@ const express = require('express')
 const Article = require('./../models/article')
 const router = express.Router()
 
+router.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 router.get('/blog', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     Article.find({}).then((article) => {
         res.send(article);
     })
